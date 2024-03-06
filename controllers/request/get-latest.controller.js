4ad577bccffba0eb0d8d3439048c59fad88092f9
Jason Addy <jasonaddy51@gmail.com>
2024-03-06 23:48:39 +0000

    a

--- controllers/request/get-latest.controller.js
+++ controllers/request/get-latest.controller.js
@@ -4,26 +4,36 @@ const verifyDriver = require("../../middleware/verifyDriver.module");
 async function getLatestRequests(req, res) {
   try {
     // Get today's pending requests that does not have the driver as part of the negotiation.driver in the negotiations array
-    const requests = await Request.find(
+    const requests = await Request.aggregate([
       {
-        status: "pending",
-        "negotiations.driver": { $ne: req.driver._id },
+        $match: {
+          status: "pending",
+        },
       },
-      null,
       {
-        sort: { createdAt: -1 },
-        limit: 5,
-      }
-    ).populate("negotiations");
+        $lookup: {
+          from: "negotiations",
+          localField: "negotiations",
+          foreignField: "_id",
+          as: "negotiations",
+        },
+      },
+      {
+        $match: {
+          "negotiations.driver": { $ne: req.driver._id },
+        },
+      },
+    ]);
 
     return res.status(200).json({
       message: "🎉 Requests retrieved successfully!!",
       data: requests,
     });
   } catch (error) {
+    console.error(error);
     return res.status(500).json({
       message: "Internal server error",
-      error: error,
+      error: JSON.stringify(error, null, 2),
     });
   }
 }
