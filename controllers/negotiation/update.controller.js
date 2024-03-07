7d94f894d9639ff2dca45739a3ec258a2b44c3de
Jason Addy <jasonaddy51@gmail.com>
2024-03-07 16:04:13 +0000

    Changes

---
+++ controllers/negotiation/update.controller.js
@@ -0,0 +1,68 @@
+const Negotiation = require("../../models/negotiation.model");
+const Request = require("../../models/request.model");
+const verifyUser = require("../../middleware/verifyUser.module");
+// const { TRIGGERS } = require("../../utils/variables");
+// const { emitToUser } = require("../../config/socket.config");
+
+async function updateNegotiation(req, res) {
+  try {
+    const { negotiation_id } = req.params;
+
+    const { status } = req.body;
+
+    if (!negotiation_id || !status)
+      return res.status(400).json({ message: "😒 Invalid request!!" });
+
+    const negotiation = await Negotiation.findById(negotiation_id).populate([
+      {
+        path: "request",
+        populate: {
+          path: "user",
+        },
+      },
+    ]);
+
+    if (!negotiation)
+      return res.status(404).json({ message: "😥 Negotiation not found" });
+
+    if (negotiation.user._id.toString() !== req.user._id.toString())
+      return res.status(403).json({ message: "😒 You are not authorized" });
+
+    if (negotiation.status === status)
+      return res.status(400).json({ message: "😒 Status already updated" });
+
+    const updatedNegotiation = await Negotiation.findByIdAndUpdate(
+      { _id: negotiation_id },
+      { status }
+    );
+
+    if (!updatedNegotiation)
+      return res.status(404).json({ message: "😥 Negotiation not found" });
+
+    if (status === "accepted") {
+      // Update request
+      const updatedRequest = await Request.findByIdAndUpdate(
+        negotiation.request._id,
+        { status: "ongoing" }
+      );
+      if (!updatedRequest)
+        return res.status(404).json({ message: "😥 Request not found" });
+    }
+
+    return res.status(200).json({
+      message: `🎉 Negotiation ${status} successfully!!`,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error,
+    });
+  }
+}
+
+module.exports = {
+  method: "post",
+  route: "/negotiation",
+  controller: [verifyUser, updateNegotiation],
+};
