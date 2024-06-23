40993541a14ca7165d6909bf1920b754bc7a18ae
Jason Addy <jasonaddy51@gmail.com>
2024-06-23 16:56:33 +0000

    chore: Refactor socketConfig function and handleConnection in socket.config.js

--- config/socket.config.js
+++ config/socket.config.js
@@ -5,7 +5,6 @@ const User = require("../models/user.model");
 const Driver = require("../models/driver.model");
 
 let io;
-let socketConnection;
 
 function socketConfig(server, PORT) {
   io = socketio(server, {
@@ -20,62 +19,57 @@ function socketConfig(server, PORT) {
 }
 
 function handleConnection(socket) {
-  try {
-    socketConnection = socket;
+  socket.on("join", async ({ token, location }) => {
+    try {
+      let { _id, exp } = jwt.verify(token, process.env.JWT_SECRET);
 
-    socket.on("join", async ({ token, location }) => {
-      try {
-        let { _id, exp } = jwt.verify(token, process.env.JWT_SECRET);
-
-        // Check if token is expired
-        if (Date.now() > exp * 1000) {
-          socket.emit("error", { message: "Token expired" });
-          return;
-        }
-
-        // Check if user or driver exists and join the appropriate room
-        const user = await User.findOne({ _id });
-        const driver = await Driver.findOne({ _id });
-
-        if (!user && !driver) {
-          socket.emit("error", { message: "User not found" });
-          return;
-        }
-
-        connectedUsers[_id] = socket.id;
-
-        if (user) {
-          user.online = true;
-          user.locationHistory.push({
-            lat: location?.coords.latitude,
-            lng: location?.coords.longitude,
-            description: location?.description,
-          });
-          await user.save();
-        }
-
-        if (driver) {
-          driver.online = true;
-          driver.locationHistory.push({
-            lat: location?.coords.latitude,
-            lng: location?.coords.longitude,
-            description: location?.description,
-          });
-          await driver.save();
-        }
-        console.log("Socket connection established");
-        console.log(connectedUsers);
-      } catch (error) {
-        console.error("Error during user join:", error);
-        socket.emit("error", { message: "Internal server error" });
+      // Check if token is expired
+      if (Date.now() > exp * 1000) {
+        socket.emit("error", { message: "Token expired" });
+        return;
       }
-    });
 
-    socket.on("disconnect", handleDisconnect);
-  } catch (error) {
-    console.log(error);
-  }
+      // Check if user or driver exists and join the appropriate room
+      const user = await User.findOne({ _id });
+      const driver = await Driver.findOne({ _id });
+
+      if (!user && !driver) {
+        socket.emit("error", { message: "User not found" });
+        return;
+      }
+
+      connectedUsers[_id] = socket.id;
+
+      if (user) {
+        user.online = true;
+        user.locationHistory.push({
+          lat: location?.coords.latitude,
+          lng: location?.coords.longitude,
+          description: location?.description,
+        });
+        await user.save();
+      }
+
+      if (driver) {
+        driver.online = true;
+        driver.locationHistory.push({
+          lat: location?.coords.latitude,
+          lng: location?.coords.longitude,
+          description: location?.description,
+        });
+        await driver.save();
+      }
+      console.log("Socket connection established");
+      console.log(connectedUsers);
+    } catch (error) {
+      console.error("Error during user join:", error);
+      socket.emit("error", { message: "Internal server error" });
+    }
+  });
+
+  socket.on("disconnect", handleDisconnect);
 }
+
 async function handleDisconnect(socket) {
   const userId = Object.keys(connectedUsers).find(
     (key) => connectedUsers[key] === socket.id
@@ -85,15 +79,19 @@ async function handleDisconnect(socket) {
 
   try {
     delete connectedUsers[userId];
-    const user = await User.findByIdAndUpdate(userId, { online: false }, {
-      new: true,
-    });
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { online: false },
+      {
+        new: true,
+      }
+    );
 
     if (!user) {
       throw new Error("User not found");
     }
   } catch (error) {
-    socketConnection.emit("error", { message: "Internal server error" });
+    socket.emit("error", { message: "Internal server error" });
   }
 }
 
@@ -102,23 +100,36 @@ function emit(event, data) {
 }
 
 function emitToUser(userId, event, data, userType) {
-  console.log(connectedUsers[userId], userType);
-  socketConnection.to(connectedUsers[userId]).emit(event, data);
+  const socketId = connectedUsers[userId];
+  console.log(socketId, userType);
+  if (socketId) {
+    io.to(socketId).emit(event, data);
+  }
 }
 
 function emitToAllExceptUser(userId, event, data) {
-  socketConnection.broadcast.to(connectedUsers[userId]).emit(event, data);
+  const socketId = connectedUsers[userId];
+  if (socketId) {
+    io.sockets.sockets.forEach((socket) => {
+      if (socket.id !== socketId) {
+        socket.emit(event, data);
+      }
+    });
+  }
 }
 
-function joinRoom(roomId) {
-  socketConnection.join(roomId);
+function joinRoom(roomId, socketId) {
+  const socket = io.sockets.sockets.get(socketId);
+  if (socket) {
+    socket.join(roomId);
+  }
 }
+
 const socketService = {
   socketConfig,
   emit,
   emitToUser,
   emitToAllExceptUser,
-  io,
   joinRoom,
 };
 
